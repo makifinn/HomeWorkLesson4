6c772b183d017f28ab61b2b0e3bcf8f72afdcaa9
Maki <kononenkotochka@gmail.com>
2023-02-13 01:22:03 +0500

    Added Third Task

--- Program.cs
+++ Program.cs
@@ -3,8 +3,12 @@
 while (IsWork)
 
 {
-    Console.WriteLine("Для завершения работы введите: -1");
-    Console.Write("Введите номер задачи ");
+    Console.WriteLine("Выберите программу:");
+    Console.WriteLine("1 - Программа возводит первое число в натуральную степень второго числа");
+    Console.WriteLine("2 - Программа выдаёт сумму цифр в заданном числе");
+    Console.WriteLine("3 - Программа создаёт массив согласно введённым параметрам");
+    Console.WriteLine("-1 - Для завершения работы");
+    Console.Write("Введите номер программы: ");
 
     if (int.TryParse(Console.ReadLine(), out int i))
     {
@@ -43,50 +47,86 @@ while (IsWork)
                             counter *= firstnum;
                         }
 
-                        Console.WriteLine($"{counter}");
+                        Console.WriteLine($"{firstnum} в степени {secondnum} = {counter}");
                     }
                     break;
                 }
             case 2:
-            {
-                Task_2();
-
-                void Task_2()
                 {
-                    int number = ReadInt("число");
+                    Task_2();
 
-                    Calculate2(number);
-                }
-
-                int ReadInt(string argument)
-                {
-                    Console.Write($"Введите {argument}: ");
-                    int number;
-
-                    while(!int.TryParse(Console.ReadLine(),out number))
+                    void Task_2()
                     {
-                        Console.WriteLine("вы ввели не число");
-                    }
-                    
-                    return number;
-                }
+                        int number = ReadInt("число");
 
-                void Calculate2(int number)
-                {
-                    string digitString = number.ToString();
-                    int len = digitString.Length;
-                    int counter = 0;
-                    int temp = number;
-                    for (int i = 0; i < len; i ++)
+                        Calculate2(number);
+                    }
+
+                    int ReadInt(string argument)
                     {
-                        counter = counter + temp % 10;
-                        temp = temp / 10;
-                    }
-                    Console.WriteLine($"{counter}");
-                }
-                break;
-            }
+                        Console.Write($"Введите {argument}: ");
+                        int number;
 
+                        while(!int.TryParse(Console.ReadLine(),out number))
+                        {
+                            Console.WriteLine("вы ввели не число");
+                        }
+                        
+                        return number;
+                    }
+
+                    void Calculate2(int number)
+                    {
+                        string digitString = number.ToString();
+                        int len = digitString.Length;
+                        int counter = 0;
+                        int temp = number;
+                        for (int i = 0; i < len; i ++)
+                        {
+                            counter = counter + temp % 10;
+                            temp = temp / 10;
+                        }
+                        Console.WriteLine($"сумма цифр в числе {number} = {counter}");
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    Task_3();
+
+                    void Task_3()
+                    {
+                        int[] Array = GetArray(ReadInt("размер массива"), ReadInt("минимальное значение элемента массива"), ReadInt("максимальное значение элемента массива"));
+                        Console.WriteLine($"создан массив чисел: {string.Join(", ", Array)}");
+                    }
+
+                    int ReadInt(string argument)
+                    {
+                        Console.Write($"Введите {argument}: ");
+                        int number;
+
+                        while(!int.TryParse(Console.ReadLine(),out number))
+                        {
+                            Console.WriteLine("вы ввели не число");
+                        }
+                        
+                        return number;
+                    }
+
+                    int[] GetArray(int lenght, int min, int max)
+                    {
+                        int[] array = new int[lenght];
+                        Random rnd = new Random();
+                        
+                        for (int i = 0; i < lenght; i++)
+                        {
+                            array[i] = rnd.Next(min, max + 1);
+                        }
+
+                        return array;
+                    }
+                    break;
+                }
             case -1: IsWork = false; break;
         }
     }
